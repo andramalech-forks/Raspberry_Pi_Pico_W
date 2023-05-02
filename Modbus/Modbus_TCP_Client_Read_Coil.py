ba6a5c4783b289e2768840f23eba2266720ea899
Fusion Automate <eng.innovativ@gmail.com>
2023-05-02 09:04:30 +0530

    Update Modbus_TCP_Client_Read_Coil.py

--- Modbus/Modbus_TCP_Client_Read_Coil.py
+++ Modbus/Modbus_TCP_Client_Read_Coil.py
@@ -27,10 +27,10 @@ while True:
 port = 502            # port to listen to
 slave_addr = 1        # bus address of modbus_client
 coil_address = 0      # Starting Coil Address
-coil_qty = 10         # Number of Coils to Read in Single Request
+coil_qty = 5         # Number of Coils to Read in Single Request
 
 # IP Address of Modbus TCP Server
-ip = '192.168.29.221'
+ip = '192.168.1.5'
 
 # Setup Modbus TCP Client
 modbus_client = ModbusTCPMaster(slave_ip=ip,slave_port=port,timeout=5)
@@ -38,5 +38,4 @@ print(f'Updating data to Modbus TCP Server at {ip}:{port}')
        
 # READ COILS
 coil_status = modbus_client.read_coils(slave_addr=slave_addr,starting_addr=coil_address,coil_qty=coil_qty)
-print('Status of COIL from {} to {} : {}'.format(coil_address, coil_address + coil_qty, coil_status))
-
+print('Status of COIL from {} to {} : {}'.format(coil_address, coil_address + coil_qty, coil_status[::-1]))
